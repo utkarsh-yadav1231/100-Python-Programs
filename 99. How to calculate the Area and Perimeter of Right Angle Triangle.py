4fc1f3a3513804b22631f0af16feb01e7a5513c4
Utkarsh Yadav <utkarshyadav1231@gmail.com>
2020-06-19 16:22:29 +0530

    Initial Commit

---
+++ 99. How to calculate the Area and Perimeter of Right Angle Triangle.py
@@ -0,0 +1,11 @@
+from random import random
+
+x = random() *900+100
+x = int(x)
+print (x)
+
+y = int (s[0])     #x //100
+z = int (s[1])     #(x //10) % 10
+w = int (s[2])     #x % 10
+
+print (y+z+w)
